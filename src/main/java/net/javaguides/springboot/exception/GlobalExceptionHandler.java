31467c647bfa471eae6e553c49acf80b2172b224
Kristinev7 <kveneles@gmail.com>
2023-11-25 21:00:11 -0500

    Handling Specific Custom Exception using ErrorDetails, GlobalExceptionHandler, and ResourceNotFoundException classes

---
+++ src/main/java/net/javaguides/springboot/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package net.javaguides.springboot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice //handle exceptions globally
+public class GlobalExceptionHandler {
+//    annotation used to handle the specific exceptions and sending the custom response to the client
+    @ExceptionHandler(ResourceNotFoundException.class)
+//    handle specific exceptions with respect to the controller
+        public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest) {
+            ErrorDetails errorDetails = new ErrorDetails(
+                    LocalDateTime.now(),
+                    exception.getMessage(),
+                    webRequest.getDescription(false),
+                    "USER_NOT_FOUND"
+            );
+            return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+}
