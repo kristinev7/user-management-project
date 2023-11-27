38e80954d1e7acbf879b985d2d3637ccba3e04e5
Kristinev7 <kveneles@gmail.com>
2023-11-27 15:59:23 -0500

    swagger ui dependency and swagger annotations added to provide informaton in swaggerui, no need to use Postman

--- src/main/java/net/javaguides/springboot/controller/UserController.java
+++ src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -1,5 +1,8 @@
 package net.javaguides.springboot.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import net.javaguides.springboot.dto.UserDto;
@@ -15,40 +18,103 @@ import org.springframework.web.context.request.WebRequest;
 import java.time.LocalDateTime;
 import java.util.List;
 
+// tag annotation from swagger, provides information for user resource
+@Tag(
+        name= "CRUD REST APIs for User Resource",
+        description = "CRUD REST APIs = Create User, Update User, Get User, Get All Users, Delete User"
+
+)
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/users")
 public class UserController {
     private UserService userService;
 
-    // build create User REST API
+    /* BUILD CREATE USER REST API*/
+
+    // operation annotation from swagger provides summary attribute
+    @Operation (
+            summary = "Create User REST API",
+            description = "Create User REST API is used to save user in a database"
+    )
+    //ApiResponse annotation provide information about the rest ApiResponse
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping
     public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto user) {
         UserDto savedUser = userService.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
-    // build get user by id REST API
+
+    /* BUILD GET USER BY ID REST API*/
+
+    // operation annotation from swagger provides summary attribute
+    @Operation (
+            summary = "Get User By ID REST API",
+            description = "Get User By ID REST API is used to get a single user from the database"
+    )
+    //ApiResponse annotation provide information about the rest ApiResponse
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping("{id}") //http://localhost:8080/api/users/{id}
     public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long userId){
         UserDto user = userService.getUserById(userId);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    // build get all users REST API
+    /* BUILD GET ALL USER REST API*/
+
+    // operation annotation from swagger provides summary attribute
+    @Operation (
+            summary = "Get All Users REST API",
+            description = "Get All Users REST API is used to get all the users from the database"
+    )
+    //ApiResponse annotation provide information about the rest ApiResponse
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @GetMapping //http://localhost:8080/api/users
     public ResponseEntity<List<UserDto>> getAllUsers() {
         List<UserDto> users = userService.getAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
-    // build update user REST API
+    /* BUILD UPDATE USER REST API */
+
+    // operation annotation from swagger provides summary attribute
+    @Operation (
+            summary = "Update User REST API",
+            description = "Update User REST API is used to update a particular user from the database"
+    )
+    //ApiResponse annotation provide information about the rest ApiResponse
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @PutMapping("{id}") // http://localhost:8080/api/users/{id} //incoming requests
     public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long userId, @RequestBody @Valid UserDto user){
         user.setId(userId);
         UserDto updatedUser = userService.updateUser(user);
         return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
-    // build delete user REST API
+
+    /* BUILD DELETE USER REST API */
+
+    // operation annotation from swagger provides summary attribute
+    @Operation (
+            summary = "Delete User REST API",
+            description = "Delete User REST API is used to delete a particular user from the database"
+    )
+    //ApiResponse annotation provide information about the rest ApiResponse
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @DeleteMapping("{id}") //http://localhost:8080/api/users/{id}
     public ResponseEntity<String> deleteUser(@PathVariable("id") Long userId){
         userService.deleteUser(userId);
--- src/main/java/net/javaguides/springboot/dto/UserDto.java
+++ src/main/java/net/javaguides/springboot/dto/UserDto.java
@@ -1,5 +1,6 @@
 package net.javaguides.springboot.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -7,16 +8,32 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+//swagger annotation to provide information in swagger ui
+@Schema(
+        description = "UserDto Model Information"
+)
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
     private Long id;
+
+    @Schema(
+            description = "User First Name"
+    )
     @NotEmpty(message = "user first name should not be empty or null") /* user first name should not be empty or null */
     private String firstName;
+
+    @Schema(
+            description = "User Last Name"
+    )
     @NotEmpty(message="user last name should not be empty or null")/* user last name should not be empty or null */
     private String lastName;
+
+    @Schema(
+            description = "User Email Address"
+    )
     @NotEmpty(message="user email should not be empty or null") /* user email should not be empty or null */
     @Email(message="email address must be valid")/*email add must be valid*/
     private String email;
